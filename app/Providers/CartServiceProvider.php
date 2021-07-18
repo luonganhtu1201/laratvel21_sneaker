feac8b78b84635d903fef4091d983b5a58aaf389
luonganhtu1201 <latu.hubt@gmail.com>
2021-07-18 10:37:32 +0700

    p1

--- app/Providers/CartServiceProvider.php
+++ app/Providers/CartServiceProvider.php
@@ -25,7 +25,15 @@ class CartServiceProvider extends ServiceProvider
     public function boot()
     {
         view()->composer([
-            'frontend.includes.header','frontend.layouts.master'
+            'frontend.includes.header',
+            'frontend.layouts.master',
+            'frontend.client.home',
+            'frontend.client.cart',
+            'frontend.client.category-product',
+            'frontend.client.profile',
+            'frontend.client.single-product',
+            'frontend.client.statusCart',
+
         ],CartProductComposer::class);
     }
 }
