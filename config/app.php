6822b5aea3284ef73b831065ea0bbc2807533ac4
luonganhtu1201 <latu.hubt@gmail.com>
2021-07-18 11:43:49 +0700

    p5

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://tunzsneaker.herokuapp.com'),
+    'url' => env('APP_URL', 'http://tunzsneaker.herokuapp.com'),
 
     'asset_url' => env('ASSET_URL', null),
 
